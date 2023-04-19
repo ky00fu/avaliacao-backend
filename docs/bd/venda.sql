daf2bfa0e84ac7f70fd66719684932cc915940a8
雪 <110499473+ky00fu@users.noreply.github.com>
2023-04-19 11:30:42 -0300

    Update venda.sql

--- docs/bd/venda.sql
+++ docs/bd/venda.sql
@@ -23,3 +23,28 @@ CREATE TABLE vendas (
     FOREIGN KEY (produtoID) REFERENCES produtos(id),
     FOREIGN KEY (vendedorID) REFERENCES vendedores(id)
 );
+
+DROP VIEW IF EXISTS vw_vendas_vendedores;
+CREATE VIEW vw_vendas_vendedores AS
+SELECT vendedores.nome AS nome_vendedor,
+       vendedores.matricula,
+       SUM(vendas.quantidade * produtos.valor) AS total_vendido,
+       SUM(vendas.quantidade * produtos.valor * 0.05) AS comissao
+FROM vendas
+JOIN produtos ON vendas.produtoID = produtos.id
+JOIN vendedores ON vendas.vendedorID = vendedores.id
+GROUP BY vendedores.id;
+
+DROP VIEW IF EXISTS vw_vendas;
+CREATE VIEW vw_vendas AS
+SELECT v.data AS data_venda, p.nome AS nome_produto, ve.nome AS nome_vendedor
+FROM vendas v
+JOIN produtos p ON v.produtoID = p.id
+JOIN vendedores ve ON v.vendedorID = ve.id;
+
+DROP VIEW IF EXISTS vendas_por_produto;
+CREATE VIEW vendas_por_produto AS
+SELECT p.nome AS produto, SUM(v.quantidade * p.valor) AS total_vendas
+FROM produtos p
+JOIN vendas v ON p.id = v.produtoID
+GROUP BY p.id;
